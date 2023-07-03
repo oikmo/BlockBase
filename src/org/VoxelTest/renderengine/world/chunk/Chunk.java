3f2073d9f3fca2a563da0321f3f84b2c08ff1791
Oikmo <genisis344@gmail.com>
2023-07-03 22:20:14 +0100

    Update Chunk.java

--- src/org/VoxelTest/renderengine/world/chunk/Chunk.java
+++ src/org/VoxelTest/renderengine/world/chunk/Chunk.java
@@ -18,6 +18,22 @@ public class Chunk {
 		this.origin = origin;
 	}	
 	
+	private void generateChunk() {
+        // Generate blocks for the chunk, you can use procedural generation or load from file.
+        for (int x = 0; x < CHUNK_SIZE; x++) {
+            for (int y = 0; y < CHUNK_SIZE; y++) {
+                for (int z = 0; z < CHUNK_SIZE; z++) {
+                    // Example: Fill the bottom layer with grass, rest with stone
+                    if (y == 0) {
+                        blocks[x][y][z] = new Block(x, y, z, Block.Type.GRASS);
+                    } else {
+                        blocks[x][y][z] = new Block(x, y, z, Block.Type.STONE);
+                    }
+                }
+            }
+        }
+    }
+	
 	public List<Block> getBlocks() {
 		return blocks;
 	}
